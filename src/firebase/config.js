d5c65c26a9edbb4f8e904d6b249be692ec40db7d
miyasatotakafumi <grassroots0923@gmail.com>
2020-06-11 21:53:04 +0900

    new

---
+++ src/firebase/config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyAralimhYqUNqxbF2DnpZSFK8gB1_FahV0",
+    authDomain: "todolist-5e7a7.firebaseapp.com",
+    databaseURL: "https://todolist-5e7a7.firebaseio.com",
+    projectId: "todolist-5e7a7",
+    storageBucket: "todolist-5e7a7.appspot.com",
+    messagingSenderId: "536668164418",
+};
