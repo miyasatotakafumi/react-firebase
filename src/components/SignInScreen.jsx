7de1a4bda5781898f7e60a35d882fbcfb1d75c9b
miyasatotakafumi <41817171+miyasatotakafumi@users.noreply.github.com>
2020-06-12 17:19:26 +0900

    fb deleted

--- src/components/SignInScreen.jsx
+++ src/components/SignInScreen.jsx
@@ -7,10 +7,10 @@ const uiConfig = {
     signInSuccessUrl: "/",
     signInOptions: [
         // firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-        firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+        // firebase.auth.FacebookAuthProvider.PROVIDER_ID,
         // firebase.auth.TwitterAuthProvider.PROVIDER_ID,
         // firebase.auth.GithubAuthProvider.PROVIDER_ID,
-        firebase.auth.EmailAuthProvider.PROVIDER_ID,
+        firebase.auth.EmailAuthProvider.PROVIDER_ID
         // firebase.auth.PhoneAuthProvider.PROVIDER_ID
         // firebaseui.auth.AnonymousAuthProvider.PROVIDER_ID
     ],
