57a7c81cb76511c9569c5cafc1d4cdbb571baf76
miyasatotakafumi <41817171+miyasatotakafumi@users.noreply.github.com>
2020-06-12 17:22:05 +0900

    changed

--- src/components/SignInScreen.jsx
+++ src/components/SignInScreen.jsx
@@ -7,7 +7,7 @@ const uiConfig = {
     signInSuccessUrl: "/",
     signInOptions: [
         // firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-        // firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+         firebase.auth.FacebookAuthProvider.PROVIDER_ID,
         // firebase.auth.TwitterAuthProvider.PROVIDER_ID,
         // firebase.auth.GithubAuthProvider.PROVIDER_ID,
         firebase.auth.EmailAuthProvider.PROVIDER_ID
